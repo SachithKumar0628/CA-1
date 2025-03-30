4b00a8608b82b8c8277b9fa8a440a5d7d10b21e8
SachithKumar0628 <sachith.k@s.amity.edu>
2025-03-30 22:04:11 +0530

    Basic C program

---
+++ Btech.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    printf("Select an option:\n");
+    printf("1. B.Tech (CSE)\n");
+    printf("2. B.Tech (AIML)\n");
+    printf("3. B.Tech (ECE)\n");
+    printf("4. B.Tech (Mechanical)\n");
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+	    case 1:
+            printf("Hello, World!\n");
+            break;
+	    }
+    return 0;
+}
+:
+
